52025f5817884902fdc11b0baaa43631351bcab6
Jan Hric <hrj@panter.ch>
2024-11-25 14:23:12 +0100

    Initial commit

---
+++ app/controllers/taxes_controller.rb
@@ -0,0 +1,9 @@
+class TaxesController < ApplicationController
+  def calculator
+    # render the calculator form
+  end
+
+  def calculate
+    # calculate taxes and render result
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  resource :tax, only: [] do
+    get :calculator
+    post :calculate
+  end
+end
