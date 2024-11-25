8b2726c2b34af8ecd855f05b7c8221c5aa776c7d
Jan Hric <hrj@panter.ch>
2024-11-25 15:06:00 +0100

    change home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '', to: 'taxes#calculator'
 
   resource :tax, only: [] do
-    get :calculator
     post :calculate
   end
 end
